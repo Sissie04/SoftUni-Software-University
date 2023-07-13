a23f9a25dfff1069b7a6b2b57811a6eef18bcb34
Sissie <sinemdeniz2610@gmail.com>
2023-07-13 21:52:10 +0300

    Add new exercise

---
+++ Programming basics with JavaScript/10_While_Loop - Exercise/07_moving.js
@@ -0,0 +1,43 @@
+function moving(input) {
+    let index = 0;
+
+    let width = Number(input[index]);
+    index++;
+
+    let length = Number(input[index]);
+    index++;
+
+    let height = Number(input[index]);
+    index++;
+
+    let volume = width * length * height;
+
+    let boxes = input[index];
+    index++;
+
+    while (boxes !== 'Done') {
+        boxes = Number(boxes);
+
+        volume -= boxes;
+
+        if (volume <= 0) {
+            console.log(`No more free space! You need ${Math.abs(volume)} Cubic meters more.`);
+            return;
+        }
+        boxes = input[index];
+        index++;
+    }
+
+    console.log(`${volume} Cubic meters left.`);
+
+
+}
+moving([
+    "10",
+    "10",
+    "2",
+    "20",
+    "20",
+    "20",
+    "20",
+    "122"]);
